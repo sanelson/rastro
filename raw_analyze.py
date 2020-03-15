dfaef00daa54e6a3f4bb566c7a723c006cee9316
Sam Nelson <sanelson@siliconfuture.net>
2020-03-15 13:19:01 -0600

    First version with working 4 color plane histogram

--- raw_analyze.py
+++ raw_analyze.py
@@ -7,7 +7,37 @@ from matplotlib import pyplot as plt
 import numpy as np
 import numpy.ma as ma
 import PIL
-#import numpy.ndarray as nd
+
+def get_color_shade(color_plane_name):
+  """
+    Return a floating point RGB color array definition from a color plane name.
+    Multiple instances of the same color plane have their color shifted using the color
+    plane index.
+  """
+  # Only supports RGB image color planes for now
+  color_shade = color_plane_name[0].upper()
+
+  # Check for color plane index
+#  color_shade_instance = 1
+  try:
+    color_shade_instance = int(color_plane_name[1])
+  except IndexError:
+    color_shade_instance = 1
+
+  color_shades_map = {
+      "R": [1.0, 0, 0],
+      "G": [0, 1.0, 0],
+      "B": [0, 0, 1.0]
+  }
+
+  #color_shade_rgb = color_shades_map[color_shade] / float(color_shade_instance)
+  color_shade_rgb = [rgb_shade / float(color_shade_instance) for rgb_shade in color_shades_map[color_shade]]
+
+  return color_shade_rgb
+
+# High DPI screen plot resolution hack
+# TODO: figure out how to add some intelligence for handling different display DPIs
+mpl.rcParams['figure.dpi'] = 300
 
 # Links:
 # https://docs.scipy.org/doc/numpy/reference/maskedarray.generic.html
@@ -63,10 +93,11 @@ with rawpy.imread(raw_file) as raw:
 
   # Extract individual color planes
 #  color_plane = np.copy(raw.raw_image_visible)
+  color_planes = list(range(color_plane_count))
   for i in range(color_plane_count):
     print("Working on color plane [{}] [{}]".format(str(i), color_plane_idx[i]))
     mask = (raw.raw_colors_visible != i)
-    print('mask: \n', mask)
+    #print('mask: \n', mask)
     #color_plane = np.copy(raw.raw_colors_visible)
     #color_plane = ma.masked_array(np.copy(raw.raw_image_visible), mask)
     color_plane_masked = ma.masked_array(np.copy(raw.raw_image_visible), mask)
@@ -77,19 +108,32 @@ with rawpy.imread(raw_file) as raw:
 #    color_plane = ma.masked_array(np.copy(raw.raw_image_visible), mask)
     #print("Shape: ", color_plane.shape)
     half_size_shape = tuple(int(dim/2) for dim in raw.raw_image_visible.shape)
-    print("Half-Size Shape: ", half_size_shape)
+    #print("Half-Size Shape: ", half_size_shape)
 
     color_plane_1d = color_plane_masked[~color_plane_masked.mask]
-    print("Shape: ", color_plane_1d.shape)
+    #print("Shape: ", color_plane_1d.shape)
 #    color_plane = ma.masked_array(np.copy(raw.raw_image_visible), mask)
-    print('Color plane data: \n', color_plane_1d.data)
+    #print('Color plane data: \n', color_plane_1d.data)
     
     # Reshape the array
     # TODO: Is this the best way?
     color_plane_2d = np.reshape(color_plane_1d, half_size_shape)
-    print('Reshaped Color plane data: \n', color_plane_2d.data)
+    #print('Reshaped Color plane data: \n', color_plane_2d.data)
 
-    print("2D Shape: ", color_plane_2d.shape)
+    #print("2D Shape: ", color_plane_2d.shape)
+
+    # Plot a basic histogram of this color plane
+
+    print("Color shade: ", get_color_shade(color_plane_idx[i]))
+    #plt.hist(color_plane_2d, bins = range(0, 2^14, 1))
+    #plt.hist(color_plane_2d, bins = range(0, 255, int((2**14/256)/2)))
+    #plt.hist(color_plane_1d, bins = range(0, 255, int((2**14/256)/2)))
+    #plt.hist(color_plane_1d, 256, density=True, facecolor='g', alpha=0.75)
+    #plt.hist(color_plane_1d, 256, density=False, facecolor='g', alpha=0.75)
+    #plt.hist(color_plane_1d, 256, density=False, facecolor=color_plane_idx[i][0].lower(), alpha=0.75)
+    plt.hist(color_plane_1d, 1024, density=False, facecolor=get_color_shade(color_plane_idx[i]), alpha=0.75, label=color_plane_idx[i])
+    #plt.title("Image Histogram")
+    #plt.show()
 
     #plt.imshow(color_plane.data, interpolation='nearest')
     #plt.imshow(color_plane_2d, interpolation='nearest')
@@ -97,7 +141,13 @@ with rawpy.imread(raw_file) as raw:
     
     #plt.savefig("{}.{}.{}".format(raw_file, color_plane_idx[i], "png"))
     #plt.imsave("{}.{}.{}".format(raw_file, color_plane_idx[i], "png"), color_plane_2d)
-    plt.imsave("{}.{}.{}".format(raw_file, color_plane_idx[i], "tiff"), color_plane_2d)
+#    plt.imsave("{}.{}.{}".format(raw_file, color_plane_idx[i], "tiff"), color_plane_2d)
+
+  plt.title("Image Histogram")
+#  plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+#  plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left', ncol=2, mode="expand", borderaxespad=0.)
+  plt.legend()
+  plt.show()
 
 ##  rgb = raw.postprocess()
 #  raw_image = raw.raw_image_visible
@@ -109,4 +159,5 @@ with rawpy.imread(raw_file) as raw:
 #  print(raw.sizes)
 #  print(raw_image.shape)
 #  print(raw_image[1,2])
+
 
